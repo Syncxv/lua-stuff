0205afd6150a7a7985a56a91d473ef47f9381398
Syncxv <aruldevarackal1@gmail.com>
2023-03-07 02:55:37 +1100

    shit

---
+++ onclick.js
@@ -0,0 +1,13 @@
+const bruh = {
+    onClick: function() {
+        return d ? p() : 0 === c.credits.premium ? c.hasActiveSubscription ? l((0,
+            w.jsx)(b, {
+                currentUser: c
+            })) : l((0,
+        w.jsx)(k, {})) : l((0,
+        w.jsx)(v, {
+            currentUser: c,
+            startChallenge: p
+        }))
+    },
+}
