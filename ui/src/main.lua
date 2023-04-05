e9de2c416f40ac347b1c0bf3919f1ce08d4c7cfd
Syncxv <aruldevarackal1@gmail.com>
2023-04-06 02:43:10 +1000

    aimbot XD

--- ui/src/main.lua
+++ ui/src/main.lua
@@ -1,12 +1,15 @@
 local esp = require("esp")
 local hitbox = require("hitbox")
+local aimbot = require("aimbot")
 
 function gui_init()
     esp.esp_core:init()
     hitbox:init()
+    aimbot:init()
     local UILibrary = require("gui")
 
     local MainUI = UILibrary.Load("Hi there")
+    aimbot:gui_init(MainUI)
     esp:gui_init(MainUI)
     hitbox:gui_init(MainUI)
 end
